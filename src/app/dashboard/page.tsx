b13bf441a25cbadb1c5108055687672bc8940303
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-10-04 15:56:43 +0600

    make admin page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from '@/components/Navbar';
 import React from 'react';
 
 const DashboardPage = () => {
     return (
         <div>
-            DashboardPage
+            <Navbar />
+            dashboard
         </div>
     );
 };
